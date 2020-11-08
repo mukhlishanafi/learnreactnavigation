6ff69de8048fa7a170fe17d3e0cacb08d95086c4
mukhlishanafi <mukhlishanafi04@gmail.com>
2020-11-08 15:58:15 +0700

    moving between screen

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 import {AppRegistry} from 'react-native';
 // import App from './App';
 // import App from './src/gettingstarted';
-import App from './src/helloreactnavigation';
+// import App from './src/helloreactnavigation';
+import App from './src/movingbetweenscreens';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
