f448df6599881197503d60382c977abe71651043
mukhlishanafi <mukhlishanafi04@gmail.com>
2020-11-08 15:02:51 +0700

    hello react navigation

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 // import App from './App';
-import App from './src/gettingstarted';
+// import App from './src/gettingstarted';
+import App from './src/helloreactnavigation';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
