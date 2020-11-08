0da9f86b04b600a82be6b3b4624b02dc475f3e53
mukhlishanafi <mukhlishanafi04@gmail.com>
2020-11-08 14:51:18 +0700

    getting started

---
+++ src/gettingstarted/index.js
@@ -0,0 +1,25 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
+
+const Stack = createStackNavigator()
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
